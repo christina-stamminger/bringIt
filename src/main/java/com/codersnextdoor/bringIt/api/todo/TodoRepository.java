90381e36bca7d2a9b2d0fddcacce90b132cab6a1
hannes <hannesku75@gmail.com>
2024-06-06 13:28:25 +0200

    implemented: GET TODO BY ID running GET TODO BY userId OFFERED, GET TODO BY userId TAKEN ... errorhandling not yet

--- src/main/java/com/codersnextdoor/bringIt/api/todo/TodoRepository.java
+++ src/main/java/com/codersnextdoor/bringIt/api/todo/TodoRepository.java
@@ -2,6 +2,16 @@ package com.codersnextdoor.bringIt.api.todo;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    @Query("SELECT t FROM Todo t WHERE t.userOffered.userId = :searchUserOffered")
+    Set<Todo> findTodoByOfferedUserId(@Param("searchUserOffered") long searchUserOffered);
+
+    @Query("SELECT t FROM Todo t WHERE t.userTaken.userId = :searchUserTaken")
+    Set<Todo> findTodoByTakenUserId(@Param("searchUserTaken") long searchUserTaken);
 }
