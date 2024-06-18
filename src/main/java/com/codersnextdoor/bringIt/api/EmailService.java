0a02b5d03b2515e7fd0409a423d330c61f7e5b05
hannes <hannesku75@gmail.com>
2024-06-18 12:52:55 +0200

    implemented: AsyncConfig Changes in: UpdateTodoController, UpdateTodoService, EmailService --> E-mail creating and sending logic changed to async.

--- src/main/java/com/codersnextdoor/bringIt/api/EmailService.java
+++ src/main/java/com/codersnextdoor/bringIt/api/EmailService.java
@@ -3,6 +3,7 @@ package com.codersnextdoor.bringIt.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendSimpleEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("bringit_service@outlook.com");
--- src/main/java/com/codersnextdoor/bringIt/api/todo/update/UpdateTodoService.java
+++ src/main/java/com/codersnextdoor/bringIt/api/todo/update/UpdateTodoService.java
@@ -4,6 +4,7 @@ import com.codersnextdoor.bringIt.api.EmailService;
 import com.codersnextdoor.bringIt.api.todo.Todo;
 import com.codersnextdoor.bringIt.api.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 
@@ -13,6 +14,7 @@ public class UpdateTodoService {
     @Autowired
     private EmailService emailService;
 
+    @Async
     public void createStatusNotificationMail (String todoStatus, Todo updateTodo, User userTaken) {
 
         String mailSubject = "";
